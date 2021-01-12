7fa5a22f4a49ceaaaac20b6046a09741352d28e6
ferry16 <guillaume.ferry@univ-lorraine.fr>
2021-01-12 09:33:27 +0100

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn.zwift.com/app/ZwiftSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'zwift*'
+  checksum      = '628EBC628672DC1618E9DAC72B18E7E6ABD90DB6CA186E4CAF4080C08C77F8EA'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
