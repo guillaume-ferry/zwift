c992bd403413e5162c3238cabb8dee815941bb75
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2022-02-11 23:23:19 +0100

    update to version 1.1.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,15 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cdn.zwift.com/app/ZwiftSetup.exe'
+﻿$ErrorActionPreference = 'Stop'
+$url = 'https://cdn.zwift.com/app/ZwiftSetup.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  softwareName  = 'zwift*'
-  checksum      = '0FA151B1ADF8813E93EFFD8154CBF3E8437D4E7D78E8AAE29DFAAF451B2D2028'
-  checksumType  = 'sha256'
-
-  silentArgs   = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'zwift*'
+  checksum       = 'F51FCCD7FB49F65724CA403B8807621F1BE06D168D58AEDB4AF8F3FCDCBC3A6B'
+  checksumType   = 'sha256'
+  silentArgs     = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
